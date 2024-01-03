0783eb0a5d25a2838ce67fa4baf4276fc820be01
lisandroguerra01 <lisandroguerra01@gmail.com>
2024-01-02 23:53:20 -0300

    add render contador del carrito

--- src/Components/CartWidget/CartWidget.jsx
+++ src/Components/CartWidget/CartWidget.jsx
@@ -1,15 +1,20 @@
 import {AiOutlineShoppingCart} from 'react-icons/ai';
 import Badge from 'react-bootstrap/Badge';
+import { useContext } from 'react';
+import { CartContext } from '../../Context/CartContext';
 
 
 // eslint-disable-next-line react/prop-types
-const CartWidget = ({items}) => {
+const CartWidget = () => {
+
+    const { getQuantity } = useContext(CartContext);
+
     return (
         <div>
             <i className='fs-2'><AiOutlineShoppingCart /></i>
             <span>
                 <Badge pill bg="success">
-                    {items}
+                    {getQuantity()}
                 </Badge>
             </span>
         </div>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,48 +4,52 @@ import CartWidget from '../CartWidget/CartWidget';
 import Logo from './Logo';
 import { NavDropdown } from 'react-bootstrap';
 import "./Header.css";
+import { Link } from 'react-router-dom';
 
 
 function NavBar() {
     return (
         <div className='header'>
             <Navbar data-bs-theme="dark" className='p-3 text-center'>
-                <Navbar.Brand href='/' className='col-4'><Logo></Logo></Navbar.Brand>
+                <Link to='/' className='col-4'><Logo></Logo></Link>
                 <Nav className="col-8 justify-content-end px-5 fs-5 fw-semibold">
                     <NavDropdown title="PRODUCTOS" id="navbarscrollingproductos" className='my-auto'>
-                        <NavDropdown.Item href='/category/sabores' className='fw-semibold'>
+                        <Link to='/category/sabores' className='fw-semibold'>
                             SABORES
-                        </NavDropdown.Item>
-                        <NavDropdown.Item href='/category/tentaciones' className='fw-semibold'>
+                        </Link>
+                        <Link to='/category/tentaciones' className='fw-semibold'>
                             TENTACIONES
-                        </NavDropdown.Item>
-                        <NavDropdown.Item href='/category/pre-envasados' className='fw-semibold'>
+                        </Link>
+                        <Link to='/category/pre-envasados' className='fw-semibold'>
                             PRE ENVASADOS
-                        </NavDropdown.Item>
+                        </Link>
                     </NavDropdown>
                     <NavDropdown title="DÓNDE ESTAMOS" id="navbarscrollingdondeestamos" className='my-auto'>
-                        <NavDropdown.Item href='#Locales' className='fw-semibold'>
+                        <Link to='#Locales' className='fw-semibold'>
                             LOCALES
-                        </NavDropdown.Item>
-                        <NavDropdown.Item href='#PuntosDeVenta' className='fw-semibold'>
+                        </Link>
+                        <Link to='#PuntosDeVenta' className='fw-semibold'>
                             PUNTOS DE VENTA
-                        </NavDropdown.Item>
+                        </Link>
                     </NavDropdown>
                     <NavDropdown title="NOSOTROS" id="navbarscrollingproductos" className='my-auto'>
-                        <NavDropdown.Item href='#QuienesSomos' className='fw-semibold'>
+                        <Link to='#QuienesSomos' className='fw-semibold'>
                             QUIENES SOMOS
-                        </NavDropdown.Item>
-                        <NavDropdown.Item href='#Novedades' className='fw-semibold'>
+                        </Link>
+                        <Link to='#Novedades' className='fw-semibold'>
                             NOVEDADES
-                        </NavDropdown.Item>
-                        <NavDropdown.Item href='#AbriTuFranquicia' className='fw-semibold'>
+                        </Link>
+                        <Link to='#AbriTuFranquicia' className='fw-semibold'>
                             ABRÍ TU FRANQUICIA
-                        </NavDropdown.Item>
-                        <NavDropdown.Item href='#Contacto' className='fw-semibold'>
+                        </Link>
+                        <Link to='/category/sabores' className='fw-semibold'>
+                            SABORES
+                        </Link>
+                        <Link to='#Contacto' className='fw-semibold'>
                             CONTACTO
-                        </NavDropdown.Item>
+                        </Link>
                     </NavDropdown>
-                    <Nav.Link href='#Carrito' className='my-auto'><CartWidget items={3} /></Nav.Link>
+                    <Nav.Link to='#Carrito' className='my-auto'><CartWidget /></Nav.Link>
                 </Nav>
             </Navbar>
         </div>
